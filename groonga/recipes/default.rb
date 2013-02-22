ea0888ef6c5e505f0d5c9a71ed8f4684615ceac0
Kouhei Sutou <kou@clear-code.com>
2013-02-22 17:18:27 +0900

    Split groonga repository recipe

--- groonga/recipes/default.rb
+++ groonga/recipes/default.rb
@@ -15,37 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if platform_family?("debian")
-  package "lsb-release" do
-    action [:install]
-  end
-
-  template "/etc/apt/sources.list.d/groonga.list" do
-    extend Chef::Mixin::ShellOut
-    source "groonga.list.erb"
-    platform = node.platform
-    apt_policy = shell_out!("apt-cache", "policy").stdout
-    if /(?:[an]=)(?:unstable|sid),/ =~ apt_policy
-      code_name = "unstable"
-    else
-      code_name = shell_out!("lsb_release", "--short", "--codename").stdout.strip
-    end
-    case platform
-    when "debian"
-      component = "main"
-    when "ubuntu"
-      component = "universe"
-    end
-    variables(:platform  => platform,
-              :code_name => code_name,
-              :component => component)
-  end
-
-  package "groonga-keyring" do
-    options("--allow-unauthenticated")
-    action [:install]
-  end
-end
+include_recipe "groonga::repository"
 
 package "groonga" do
   action [:install]
