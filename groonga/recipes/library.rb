5fa45b0535346cf4ccd4dd896cf2d681dee99211
Kouhei Sutou <kou@clear-code.com>
2014-01-24 12:28:33 +0900

    Fix recipe names

--- groonga/recipes/library.rb
+++ groonga/recipes/library.rb
@@ -1,7 +1,7 @@
 # Cookbook Name:: groonga
-# Recipe:: default
+# Recipe:: library
 #
-# Copyright 2013 Kouhei Sutou <kou@clear-code.com>
+# Copyright 2014 Kouhei Sutou <kou@clear-code.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
